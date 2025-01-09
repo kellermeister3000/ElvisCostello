2a66677302d4764dece56705e3ed99610a5c71c6
Philip Keller <philipjoelkeller@gmail.com>
2025-01-08 21:09:40 -0800

    Initial Commit

---
+++ ElvisCostello/ElvisCostelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisCostelloApp.swift
+//  ElvisCostello
+//
+//  Created by Philip Keller on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisCostelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
